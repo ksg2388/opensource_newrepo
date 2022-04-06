95f38a75d70d86ebd3246c61d58a170aa7252e73
ksg2388 <45422827+ksg2388@users.noreply.github.com>
2022-04-06 23:17:29 +0900

    Update cal.sh
    2개의 숫자 -> 배열로 다루기

--- cal.sh
+++ cal.sh
@@ -2,28 +2,29 @@
 
 num1=$(<num1.txt)
 num2=$(<num2.txt)
+arr=($num1 $num2)
 
 if [ $# -gt 0 ]; then
-	echo "...run calculater..."
-	if [ $1 == "add" ]; then
-		result=$(($num1+$num2))
-	elif [ $1 == "sub" ]; then
-		result=$(($num1-$num2))
-	elif [ $1 == "div" ]; then
-		result=$(($num1/$num2))
-	elif [ $1 == "mul" ]; then
-		result=$(($num1*$num2))
 
+	echo "...run calculater..."
+
+	if [ $1 == "add" ]; then
+		result=$((${arr[0]}+${arr[1]}))
+	elif [ $1 == "sub" ]; then
+		result=$((${arr[0]}-${arr[1]}))
+	elif [ $1 == "div" ]; then
+		result=$((${arr[0]}/${arr[1]}))
+	elif [ $1 == "mul" ]; then
+		result=$((${arr[0]}*${arr[1]}))
 	fi
+
 	echo
-	echo "num1:$num1"
-	echo "num2:$num2"
+	echo "num1:${arr[0]}"
+	echo "num2:${arr[1]}"
 	echo "op:$1"
 	echo "result:$result"
-	
 else
 	echo "...none operator parameter..."
-
 	PS3='select munu:'
 	select var in add sub div mul
 	do
@@ -31,19 +32,18 @@ else
 	done
 
 	if [ $var == "add" ]; then
-		result=$(($num1+$num2))
+		result=$((${arr[0]}+${arr[1]}))
 	elif [ $var == "sub" ]; then
-		result=$(($num1-$num2))
+		result=$((${arr[0]}-${arr[1]}))
 	elif [ $var == "div" ]; then
-		result=$(($num1/$num2))
+		result=$((${arr[0]}/${arr[1]}))
 	elif [ $var == "mul" ]; then
-		result=$(($num1*$num2))
+		result=$((${arr[0]}*${arr[1]}))
 	fi
+
 	echo
-	echo "num1:$num1"
-	echo "num2:$num2"
+	echo "num1:${arr[0]}"
+	echo "num2:${arr[1]}"
 	echo "op:$var"
 	echo "result:$result"
-
 fi
-
